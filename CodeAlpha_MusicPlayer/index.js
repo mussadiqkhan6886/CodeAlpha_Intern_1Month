26af5ac2de4282c4b7550620262f9f73d6e2c59e
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-03-06 05:47:36 +0500

    fixed bugs

--- CodeAlpha_MusicPlayer/index.js
+++ CodeAlpha_MusicPlayer/index.js
@@ -1,9 +1,12 @@
 const range = document.querySelector('#range');
 const playButton = document.querySelector('.fa-play');
-const song = document.querySelector('#song');
+let song = document.querySelector('#song');
 const container = document.querySelectorAll('.thumbnail');
 const playIcon = document.querySelectorAll('.playIcon');
-const ImgDiv = document.querySelectorAll('img-div');
+const ImgDiv = document.querySelectorAll('.img-div');
+let currentSongName = document.querySelector('#current-song-name');
+let currentAuthor = document.querySelector('#current-song-singer');
+let currentSongImage = document.querySelector('#current-song')
 range.value = 0;
 
 playIcon.forEach(icon => {
@@ -40,8 +43,34 @@ range.addEventListener('input', () => {
     playButton.classList.replace('fa-play', 'fa-pause');
 });
 
-container.forEach(thumbnail => {
-    thumbnail.addEventListener('click', (e) => {
-        console.log(e.target);
+playIcon.forEach(icon => {
+    icon.addEventListener('click', function(e){
+        let id = e.target.parentElement.id;
+        let tempImgCurrent = currentSongImage.src;
+        let tempSongName = currentSongName.innerHTML;
+        let thumbnailDiv = this.closest(".thumbnail");
+        let imgElement = thumbnailDiv.querySelector(".music-img");
+        let pElement = thumbnailDiv.querySelector(".music-name");
+
+        if(id == 'dokhtarekhaana'){
+            imgElement.src = tempImgCurrent;
+            pElement.textContent = tempSongName;
+            song.src = "songs/dokhtarekhaana.mp3";
+            currentSongName.innerHTML = 'Dokhtar e khaana';
+            currentAuthor.innerHTML = 'Sediq shabab';
+            currentSongImage.src = "thumbnails/dokhtar ekhaana.webp";
+    
+        }else if(id == 'pyarbadha'){
+            imgElement.src = tempImgCurrent;
+            pElement.textContent = tempSongName;
+            song.src = "songs/pyarbadha.mp3";
+            currentSongName.innerHTML = 'Itna na Mujh sai Pyar badha';
+            currentAuthor.innerHTML = 'Talat Mahmood';
+            currentSongImage.src = "thumbnails/pyar badha.jpg";
+
+        }
+        range.value = 0;
+        song.play();
+        playButton.classList.replace('fa-play', 'fa-pause');
     })
 })
